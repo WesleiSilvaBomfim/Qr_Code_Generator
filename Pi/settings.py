78615d14e079a293cd6a0a69c6ef3ee83966b23d
weslei silva bomfim <www.weslei@gmail.com>
2018-09-23 21:47:10 -0300

    weslei.pythonanywhere.com

--- Pi/settings.py
+++ Pi/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'Pi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'codeGenerator/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
